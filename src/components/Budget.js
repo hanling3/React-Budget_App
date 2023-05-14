733af63b5202ee241188389171e25632c6a92e1a
Hedy Yang <hedyyang1004@gmail.com>
2023-05-14 18:37:29 -0400

    Update budget
    Make Budget editable with option to increase and decrease its value by 10
    
    Add error messages for budget exceeds 20,000 or lower than the spending

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,23 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                value={budget}
+                step='10'
+                style={{ size: 10 }}
+                onChange={(e) => dispatch({ type: 'SET_BUDGET', payload: e.target.value })}
+            >
+            </input>
+        </div >
     );
 };
 export default Budget;
+// onChange={(event) => setCost(event.target.value)}
